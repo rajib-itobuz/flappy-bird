74e174e2ec7457d901d22206e1f3a505d72b0de7
Rajib Nandi <rajib-itobuz.com>
2024-02-29 01:07:21 +0530

    feat: added game logic

--- test.js
+++ test.js
@@ -1,82 +1,92 @@
-// function clogged(a) {
-//     return (b) => a * b
-
-// }
-
-// const res = clogged(5);
-
-
-// console.log(res(10));
-// console.log(res(2));
-// console.log(res(3));
-// console.log(res(8));
-
-// console.log(clogged(3)(5));
-
-// var a = 10;
-// let b = 50;
-// const c = 30;
-// {
-// let a = 30;correct var->let
-// var b = 60; incorrect let->var
-// const a = 100; correct var->const
-// incorrect const->var
-// const b = 20; correct let->const vice-versa
-
-
-
-// }
+const barsArr = [];
 const screen = document.querySelector(".screen");
+const background = document.getElementsByTagName("section")[0];
 const bars = document.querySelectorAll(".bars");
 const bird = document.querySelector(".bird");
-// let heightOfBird = Math.floor(window.innerHeight / 2);
+const score = document.querySelector("#score");
+const midPoint = { height: window.innerHeight / 2, width: Math.floor(window.innerWidth / 2+50) };
+let heightOfBird = Math.floor(midPoint.height);
 
-// const middleBar = bars[bars.length / 2].getBoundingClientRect();
-// const allowedRange = [middleBar.height * 0.32, middleBar.height * 0.72, middleBar.left];
+const speed = 50;
+let currentBar = 0;
+let currentScore=0;
 
-// bird.style.top = `${heightOfBird}px`;
+bird.style.top = `${heightOfBird}px`;
 
-// bars[bars.length / 2].style.backgroundColor = "white";
-// bars[1].style.backgroundColor = "white";
+const checkIfBirdAlive = (barTop, barBottom, barLeft) => {
+    const birdRect = bird.getBoundingClientRect();
+    if (birdRect.top > barTop && birdRect.bottom < barBottom && birdRect.right > barLeft) {
+        return true;
+    }
 
-// console.log(bars[1].getBoundingClientRect());
+    return false;
+}
 
-setInterval(() => {
-    bars.forEach(e => {
+const callnextBar = () => {
+    const middleBar = barsArr[currentBar].item.getBoundingClientRect();
+    const allowedRange = [middleBar.height * barsArr[currentBar].top / 100, middleBar.height * barsArr[currentBar].bottom / 100, middleBar.left];
+    if(!checkIfBirdAlive(...allowedRange)){
+        clearInterval(updater);
+        clearTimeout(birdDecreaseHeight);
+    }
+}
 
-    })
+const createNewBar = (bar) => {
+    const random = Math.floor(Math.random() * 40 + 20);
+    const randomBottom = random + Math.floor(Math.random() * 20 + 20);
+
+    const newObj = { top: random, bottom: randomBottom, item: bar };
+    bar.style.background = `linear-gradient(#2a9d8f ${random}%, transparent ${random}%, transparent ${randomBottom}%, #2a9d8f ${randomBottom}%)`;
+    barsArr.push(newObj);
+}
+
+
+bars.forEach(e => {
+    createNewBar(e);
+})
+
+let upcomingBarisAt = midPoint.width;
+const updater = setInterval(() => {
+    const backgroundRectValue = background.getBoundingClientRect();
+    const currentBackgroundX = backgroundRectValue.x;
+    background.style.transform = `translateX(${currentBackgroundX - speed}px)`;
+
+    const newBar = document.createElement("div");
+    newBar.classList.add("bars");
+    createNewBar(newBar);
+    background.append(newBar);
+
+    const birdRect = bird.getClientRects();
+    if (currentBackgroundX < upcomingBarisAt) { 
+        callnextBar();
+        upcomingBarisAt-=270;
+        currentBar+=1;
+    }
+
+
+
 }, 500);
 
-// const checkIfBirdAlive = (barTop, barBottom, barLeft) => {
-//     const birdRect = bird.getBoundingClientRect();
-//     console.log(birdRect.top, barTop)
-//     if (birdRect.right < barLeft && birdRect.top > barTop && birdRect.bottom < barBottom) {
-//         return true;
-//     }
 
-//     return false;
-// }
+const birdDecreaseHeight = setInterval(() => {
+    currentScore+=1;
+    console.log(score);
+    score.innerText=currentScore;
+    if (heightOfBird < window.innerHeight) {
+        heightOfBird += 5;
+        bird.style.top = `${heightOfBird}px`;
+    } else {
+        // alert("Game Over");
+        clearTimeout(birdDecreaseHeight);
+    }
+}, 100);
 
-// console.log(checkIfBirdAlive(...allowedRange));
+document.addEventListener("keypress", (e) => {
 
-// const birdDecreaseHeight = setInterval(() => {
-//     // console.log("hello");
-//     if (heightOfBird < window.innerHeight) {
-//         heightOfBird += 5;
-//         bird.style.top = `${heightOfBird}px`;
-//     } else {
-//         alert("Game Over");
-//         clearTimeout(birdDecreaseHeight);
-//     }
-// }, 100);
+    if (e.code === 'Space') {
+        heightOfBird -= 15;
+        bird.style.top = `${e.clientY}px`;
+    }
 
-// document.addEventListener("keypress", (e) => {
-
-//     // console.log(e.clientX, e.clientY);
-//     if (e.code === 'Space') {
-//         heightOfBird -= 15;
-//         bird.style.top = `${e.clientY}px`;
-//     }
-
-// })
+})
 
